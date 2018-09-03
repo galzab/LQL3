ca49b81f4ad0832d2f12ad5258464441d85c4f99
Galzab <juan@galzab.com>
2018-09-03 19:51:13 +0200

    Dia 3

---
+++ liquid/src/components/Contenedor.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                key: value
+            }
+        },
+        watch: {
+            data(newValue, oldValue) {
+                
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
