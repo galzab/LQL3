b4e77fa74bc53063eab27c794a3efcd52078883c
Galzab <juan@galzab.com>
2018-09-09 13:47:16 +0200

    Cambios dia 9

--- liquid/src/views/Home.vue
+++ liquid/src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <contenedor />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-//import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '../components/HelloWorld.vue'
+import  Contenedor  from '../components/Contenedor.vue';
 
 export default {
   name: 'home',
   components: {
-    //HelloWorld
+    HelloWorld,
+    Contenedor
   }
 }
 </script>
